be1a4fec39d40b185b7328d04edd21fbb7e21621
Gianluca Bresolin <gianbreso02@gmail.com>
2025-05-21 12:13:47 +0200

    Fix: now the voteCount is update in an idempotent way and checks if the vote's term is valid.

--- leaderElection/voteCount/voteCount.go
+++ leaderElection/voteCount/voteCount.go
@@ -1,20 +1,31 @@
 package voteCount
 
-type AddVoteSignal struct{}
-type ResetSignal struct{}
+import "log"
+
+type AddVoteArg struct {
+	term    int
+	voterID string
+}
+type ResetArg struct {
+	term int
+}
 
 type VoteCount struct {
 	voteCount  int
-	AddVoteReq chan AddVoteSignal
-	ResetReq   chan ResetSignal
+	term       int
+	voterMap   map[string]struct{}
+	AddVoteReq chan AddVoteArg
+	ResetReq   chan ResetArg
 }
 
 func NewVoteCount() *VoteCount {
-	addVoteReq := make(chan AddVoteSignal)
-	resetReq := make(chan ResetSignal)
+	addVoteReq := make(chan AddVoteArg)
+	resetReq := make(chan ResetArg)
 
 	voteCount := VoteCount{
 		voteCount:  0,
+		term:       0,
+		voterMap:   make(map[string]struct{}),
 		AddVoteReq: addVoteReq,
 		ResetReq:   resetReq,
 	}
@@ -22,10 +33,10 @@ func NewVoteCount() *VoteCount {
 	go func() {
 		for {
 			select {
-			case <-addVoteReq:
-				voteCount.addVote()
-			case <-resetReq:
-				voteCount.reset()
+			case arg := <-addVoteReq:
+				voteCount.addVote(arg)
+			case arg := <-resetReq:
+				voteCount.reset(arg)
 			}
 		}
 	}()
@@ -33,10 +44,25 @@ func NewVoteCount() *VoteCount {
 	return &voteCount
 }
 
-func (voteCount *VoteCount) addVote() {
-	voteCount.voteCount++
+func (voteCount *VoteCount) addVote(arg AddVoteArg) {
+	if arg.term != voteCount.term {
+		log.Println("Vote count add request ignored: term is not equal to current term")
+	} else {
+		if _, exists := voteCount.voterMap[arg.voterID]; exists {
+			// idempotency behavior: if the voter has already voted, do nothing
+			return
+		}
+		voteCount.voteCount++
+		voteCount.voterMap[arg.voterID] = struct{}{}
+	}
 }
 
-func (voteCount *VoteCount) reset() {
-	voteCount.voteCount = 0
+func (voteCount *VoteCount) reset(arg ResetArg) {
+	if arg.term > voteCount.term {
+		voteCount.term = arg.term
+		voteCount.voteCount = 0
+		voteCount.voterMap = make(map[string]struct{})
+	} else {
+		log.Println("Vote count reset request ignored: term is not greater than current term")
+	}
 }
