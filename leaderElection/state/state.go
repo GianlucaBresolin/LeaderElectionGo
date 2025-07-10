0cbeee158a1f10c22eaadc0f9de675548d78384c
Gianluca Bresolin <gianbreso02@gmail.com>
2025-07-11 00:00:28 +0200

    Fix: setFollower and setLeader implementation.

--- leaderElection/state/state.go
+++ leaderElection/state/state.go
@@ -1,10 +1,22 @@
 package state
 
-type FollowerSignal struct{}
+import (
+	"LeaderElectionGo/leaderElection/electionTimer"
+	"LeaderElectionGo/leaderElection/heartbeatTimer"
+)
+
+type FollowerSignal struct {
+	HeartbeatTimerRef *heartbeatTimer.HeartbeatTimer
+	ElectionTimerRef  *electionTimer.ElectionTimer
+}
 type CandidateSignal struct{}
-type LeaderSignal struct{}
+type LeaderSignal struct {
+	Term       int
+	ResponseCh chan<- bool
+}
 
 type State struct {
+	term        int
 	value       string
 	FollowerCh  chan FollowerSignal
 	CandidateCh chan CandidateSignal
@@ -22,15 +34,47 @@ func NewState() *State {
 	go func() {
 		for {
 			select {
-			case <-state.FollowerCh:
-				state.value = "follower"
+			case signal := <-state.FollowerCh:
+				state.setFollower(signal)
 			case <-state.CandidateCh:
 				state.value = "candidate"
-			case <-state.LeaderCh:
-				state.value = "leader"
+			case signal := <-state.LeaderCh:
+				state.setLeader(signal)
 			}
 		}
 	}()
 
 	return state
+}
+
+func (s *State) setFollower(signal FollowerSignal) {
+	if s.value == "leader" {
+		// if the current state is leader, stop the heartbeat timer
+		signal.HeartbeatTimerRef.StopReq <- heartbeatTimer.StopSignal{}
+		// restart the election timer
+		signal.ElectionTimerRef.ResetReq <- electionTimer.ResetSignal{}
+	}
+	s.value = "follower"
+}
+
+func (s *State) setLeader(signal LeaderSignal) {
+	switch {
+	case s.term < signal.Term:
+		s.term = signal.Term
+		s.value = "leader"
+		signal.ResponseCh <- true // send true if the term is valid
+		return
+	case s.term == signal.Term:
+		if s.value != "leader" {
+			s.value = "leader"
+			signal.ResponseCh <- true // send true if the term is valid
+			return
+		} else {
+			signal.ResponseCh <- false // already a leader for this term
+			return
+		}
+	default:
+		signal.ResponseCh <- false // send false if the term is not valid
+		return
+	}
 }
